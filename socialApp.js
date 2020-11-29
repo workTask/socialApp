7670e3a21fd0388c9122089e299dc83d20f1238d
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-11-30 00:21:48 +0200

    added middleware

--- socialApp.js
+++ socialApp.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express();
 const port = 8089;
 const postRoutes = require('./routes/post')
+const morgan = require('morgan')
+
+
+const myMiddleware = (req,res, next)=>{
+    console.log("middleware connected!!!");
+    next();
+}
 
 //routes
 app.get("/", postRoutes.getPost);
 
+//middleware
+app.use(morgan('dev'));
+app.use(myMiddleware)
+
+//
 app.listen(port, ()=>{
     console.log("Server starting");
 })
