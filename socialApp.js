025c5fda94a2ed89c1fdc67d26d9e4c4ac3f436e
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-11-30 20:00:12 +0200

    connection DB

--- socialApp.js
+++ socialApp.js
@@ -4,17 +4,25 @@ const app = express();
 const postRoutes = require('./routes/post')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 dotenv.config();
 
+//DB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(()=>console.log('DB connected'))
+
+mongoose.connection.on('error', err => {console.log(` DB conection error: ${err.message}`);
+});
+//
 
 //routes
 //app.get("/", postRoutes.getPost);
+//
 
 //middleware
 app.use(morgan('dev'));
 app.use("/", postRoutes)
 //
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT||3001, ()=>{
     console.log("Server starting");
 })
