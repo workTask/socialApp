a6c5a166352426dbcfb9ae3dae6bf60378cf7fa9
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-12-03 21:33:11 +0200

    loading data with MongoDB

--- controllers/post.js
+++ controllers/post.js
@@ -2,6 +2,7 @@ const Post = require('../models/post')
 
 exports.getPosts = (req,res)=>{
   const posts = Post.find()
+  .select("_id title body")
   .then(posts => {
      res.status(200).json({posts:posts})
    })
