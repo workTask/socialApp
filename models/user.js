8b74dee08692a59f890424a69971cb7a726db893
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-12-06 12:18:05 +0200

    added userSchema

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const uuidv1 = require('uuid/v1')
+const crypto = require('crypto')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim: ture,
+        required: true,
+    },
+    email:{
+        type: String,
+        trim: ture,
+        required: true,
+    },
+    hashed_password:{
+        type: String,
+        required: true,
+    },
+    
+    salt: String,
+        created:{
+             type: Date,
+             default: Date.now,
+    },
+    update:Date
+});
+//virtual field
+userSchema.virtual('password')
+    .set(function(password){
+        this.password = password
+        // generate s timestamp
+        this.salt = uuidv1()
+        // ecrypt password
+        this.hashed_password = this.ecryptPassword(password);
+    })
+    .get(function(){
+        return this._password;
+    })
+
+userSchema.methods={
+    ecryptPassword: function(password){
+        if(!password) return "";
+        try {
+            return crypto.createHmac ('sha1',this.salt)
+                             .update(password)
+                             .digest('hex')
+        } catch (error) {
+            return "";
+        }
+    }
+}
+module.exports = mongoose.model("User", userSchema);
--- socialApp.js
+++ socialApp.js
@@ -11,8 +11,7 @@ const exspressValidator = require('express-validator');
 dotenv.config();
 
 //DB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(()=>console.log('DB connected'))
-
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true }).then(()=>console.log('DB connected'))
 mongoose.connection.on('error', err => {console.log(` DB conection error: ${err.message}`);
 });
 //
